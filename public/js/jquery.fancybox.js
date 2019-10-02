e80922184646161b5c7760d823ee2817762beab3
krushndayshmookh <krushndayshmookh@gmail.com>
2019-10-02 23:36:30 +0530

    http => https

--- public/js/jquery.fancybox.js
+++ public/js/jquery.fancybox.js
@@ -3206,7 +3206,7 @@
 		},
 
 		// Examples:
-		// http://maps.google.com/?ll=48.857995,2.294297&spn=0.007666,0.021136&t=m&z=16
+		// https://maps.google.com/?ll=48.857995,2.294297&spn=0.007666,0.021136&t=m&z=16
 		// https://www.google.com/maps/@37.7852006,-122.4146355,14.65z
 		// https://www.google.com/maps/place/Googleplex/@37.4220041,-122.0833494,17z/data=!4m5!3m4!1s0x0:0x6c296c66619367e0!8m2!3d37.4219998!4d-122.0840572
 		gmap_place : {
