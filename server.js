56fd7bd5edb2cd9b52a25b6c112c22ad149ba6b0
krushndayshmookh <krushndayshmookh@gmail.com>
2019-10-02 22:37:01 +0530

    enable DB, 404, add certificate routes

--- server.js
+++ server.js
@@ -1,35 +1,81 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const logger = require('morgan')
 
-const app = express()
+const PORT = process.env.PORT || 3000
+const DBPORT = process.env.MONGODB_URI
+var app = express()
+
+mongoose.connect(DBPORT, { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.Promise = global.Promise
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.use(logger('dev'))
+app.use(cors())
 
 app.use(express.static(__dirname + '/public'))
 
-app.set('views', __dirname+'/views')
+app.set('views', __dirname + '/views')
 
 app.set('view engine', 'pug')
 
-const PORT = process.env.PORT || 3000
-
 // ---
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
 	res.render('index')
 })
 
-app.get('/speakers',(req,res)=>{
+app.get('/speakers', (req, res) => {
 	res.render('speakers')
 })
 
-app.get('/events',(req,res)=>{
+app.get('/events', (req, res) => {
 	res.render('events')
 })
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
 	res.render('about')
 })
 
-app.get('/schedule',(req,res)=>{
+app.get('/schedule', (req, res) => {
 	res.render('schedule')
+})
+
+// Certificates -----
+
+const certificateController = require('./controllers/certificateController')
+
+app.get('/certificates/:event', certificateController.certificates_get)
+
+app.get('/certificates', (req, res) => res.redirect('/events'))
+
+app.get('/certificate/:id', certificateController.certificate_get)
+
+// 404 ---
+
+app.use(function (req, res) {
+	res.status(404)
+
+	// respond with html page
+	if (req.accepts('html')) {
+		res.render('404', {
+			url: req.url
+		})
+		return
+	}
+
+	// respond with json
+	if (req.accepts('json')) {
+		res.send({
+			error: 'Not found'
+		})
+		return
+	}
+
+	// default to plain-text. send()
+	res.type('txt').send('Not found')
 })
 
 // ---
