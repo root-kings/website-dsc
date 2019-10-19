69f248c630fef1f8978ca6839ab3007cd5ffd46b
krushndayshmookh <krushndayshmookh@gmail.com>
2019-10-19 12:58:44 +0530

    :lipstick: :rotating_light: add materialize and prettify

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
 
-Vue.config.productionTip = false;
+import 'materialize-css'
+import 'materialize-css/dist/css/materialize.css'
+// import './assets/css/dayshmookh.layout.css'
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
